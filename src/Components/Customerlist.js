464ca30bcdb089c2e617e5179fa11f2fcaae05c5
Max HH <maxbowen1234@gmail.com>
2021-11-17 21:17:46 +0200

    some changes to customerlist

--- src/Components/Customerlist.js
+++ src/Components/Customerlist.js
@@ -2,7 +2,7 @@ import React, {useState, useEffect, useRef} from 'react';
 
 import { AgGridReact, AgGridColumn} from 'ag-grid-react';
 import { Snackbar } from '@mui/material';
-import { render } from 'react-dom';
+
 
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-material.css';
